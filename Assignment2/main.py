3d897bcc7822b4e80e7d89e13c743b946645ee66
Elias <98ille@gmail.com>
2021-11-10 21:29:05 +0100

    Changed loading method

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -2,7 +2,7 @@ from matplotlib import image
 import numpy as np
 from matplotlib.image import imread
 from matplotlib.image import imsave
-
+from PIL import Image
 
 def calculatePixelValue(imgArr, kernal):
     red = np.array([
@@ -29,13 +29,6 @@ def calculatePixelValue(imgArr, kernal):
         [imgArr[2][0][0]*kernal[2][2], imgArr[2][1][2]
             * kernal[2][1], imgArr[2][2][2]*kernal[2][2]]
     ]).sum()
-    red = red/255
-    green = green/255
-    blue = blue/255
-
-    if(imgArr[1][1][3] >= 1):
-        print(imgArr[1][1][3], "HEJ")
-
     return np.array([red, green, blue, imgArr[1][1][3]])
 
 
@@ -82,37 +75,22 @@ def apply(img, kernal):
                 imgArr[2][2] = img[y+1][x+1]
             except:
                 imgArr[2][2] = [0., 0., 0., 0.]
-            # print(imgArr[1][1])
             res = calculatePixelValue(imgArr, kernal)
             output[y][x] = res
-            # print(np.amax(res))
-            """ if y == 0:
-                if x == 0:
-                    imgArr = np.zeros((3, 3, 4))
-                    imgArr[1][1] = img[0][0]
-                    imgArr[1][2] = img[0][1]
-                    imgArr[2][1] = img[1][0]
-                    imgArr[2][2] = img[1][1]
-                    img[y][x] = calculatePixelValue(imgArr, kernal)
-                elif x == x_size-1:
-                    imgArr = np.zeros((3, 3, 4))
-                    imgArr[1][0] = img[0][x-1]
-                    imgArr[1][1] = img[0][x]
-                    imgArr[2][0] = img[1][x-1]
-                    imgArr[2][1] = img[1][x]
-                    img[y][x] = calculatePixelValue(imgArr, kernal)
-                else:
-                    imgArr = np.zeros((3, 3, 4))
-                    imgArr[1][0] = img[0][x-1]
-                    imgArr[1][1] = img[0][x]
-                    imgArr[2][0] = img[1][x-1]
-                    imgArr[2][1] = img[1][x]
-                    img[y][x] = calculatePixelValue(imgArr, kernal) """
 
     return output
 
 
 img = imread("ill.png")
+
+im = Image.open('ill.png')
+
+pixel_val = list(im.getdata())
+
+img = np.asarray(im)
+
+print(img.shape)
+
 sharp = [
     [0, -1, 0],
     [-1, 5, -1],
@@ -121,7 +99,5 @@ sharp = [
 
 s = apply(img, sharp)
 
-print(np.max(s))
-
-
-imsave("sharp.png", s)
+save = Image.fromarray(np.uint8(s)).convert('RGB')
+save.save('sharp.png')
