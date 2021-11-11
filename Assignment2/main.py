0820b4cb93168b058235369ab1fe984a2e08fb90
Elias <98ille@gmail.com>
2021-11-11 17:04:21 +0100

    Changed kernal apply loop

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -3,101 +3,65 @@ import numpy as np
 from matplotlib.image import imread
 from matplotlib.image import imsave
 from PIL import Image
-
-def calculatePixelValue(imgArr, kernal):
-    red = np.array([
-        [imgArr[0][0][0]*kernal[0][0], imgArr[0][1][0] *
-            kernal[0][1], imgArr[0][2][0]*kernal[0][2]],
-        [imgArr[1][0][0]*kernal[1][0], imgArr[1][1][0] *
-            kernal[1][1], imgArr[2][2][0]*kernal[1][2]],
-        [imgArr[2][0][0]*kernal[2][0], imgArr[2][1][0]
-            * kernal[2][1], imgArr[2][2][0]*kernal[2][2]]
-    ]).sum()
-    green = np.array([
-        [imgArr[0][0][1]*kernal[0][0], imgArr[0][1][1] *
-            kernal[0][1], imgArr[0][2][1]*kernal[0][2]],
-        [imgArr[1][0][1]*kernal[1][0], imgArr[1][1][1] *
-            kernal[1][1], imgArr[2][2][1]*kernal[1][2]],
-        [imgArr[2][0][1]*kernal[2][0], imgArr[2][1][1]
-            * kernal[2][1], imgArr[2][2][1]*kernal[2][2]]
-    ]).sum()
-    blue = np.array([
-        [imgArr[0][0][0]*kernal[0][2], imgArr[0][1][2] *
-            kernal[0][1], imgArr[0][2][2]*kernal[0][2]],
-        [imgArr[1][0][0]*kernal[1][2], imgArr[1][1][2] *
-            kernal[1][1], imgArr[2][2][2]*kernal[1][2]],
-        [imgArr[2][0][0]*kernal[2][2], imgArr[2][1][2]
-            * kernal[2][1], imgArr[2][2][2]*kernal[2][2]]
-    ]).sum()
-    return np.array([red, green, blue, imgArr[1][1][3]])
+from numpy.core.fromnumeric import shape
+import cv2
+from skimage.exposure import rescale_intensity
+from skimage.exposure.exposure import _output_dtype
 
 
-def apply(img, kernal):
-    x_size, y_size, _ = img.shape
-    output = np.zeros((x_size, y_size, 4))
+def convolve(img, kernel):
+    iH, iW = img.shape[:2]
+    kH, kW = kernel.shape[:2]
+    pad = (kW - 1) // 2
+    img = cv2.copyMakeBorder(img, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    output = np.zeros((iH, iW), dtype="float32")
 
-    for y, val in enumerate(img):
-        for x, val in enumerate(val):
-            imgArr = np.zeros((3, 3, 4))
-            try:
-                imgArr[0][0] = img[y-1][x-1]
-            except:
-                imgArr[0][0] = [0., 0., 0., 0.]
-            try:
-                imgArr[0][1] = img[y-1][x]
-            except:
-                imgArr[0][1] = [0., 0., 0., 0.]
-            try:
-                imgArr[0][2] = img[y-1][x+1]
-            except:
-                imgArr[0][2] = [0., 0., 0., 0.]
-            try:
-                imgArr[1][0] = img[y][x-1]
-            except:
-                imgArr[1][0] = [0., 0., 0., 0.]
-            try:
-                imgArr[1][1] = img[y][x]
-            except:
-                imgArr[1][1] = [0., 0., 0., 0.]
-            try:
-                imgArr[1][2] = img[y][x+1]
-            except:
-                imgArr[1][2] = [0., 0., 0., 0.]
-            try:
-                imgArr[2][0] = img[y+1][x-1]
-            except:
-                imgArr[2][0] = [0., 0., 0., 0.]
-            try:
-                imgArr[2][1] = img[y+1][x]
-            except:
-                imgArr[2][1] = [0., 0., 0., 0.]
-            try:
-                imgArr[2][2] = img[y+1][x+1]
-            except:
-                imgArr[2][2] = [0., 0., 0., 0.]
-            res = calculatePixelValue(imgArr, kernal)
-            output[y][x] = res
-
+    for y in np.arange(pad, iH + pad):
+        for x in np.arange(pad, iW + pad):
+            roi = img[y - pad:y + pad + 1, x - pad:x + pad + 1]
+            k = (roi * kernel).sum()
+            output[y - pad, x - pad] = k
+    output = rescale_intensity(output, in_range=(0, 255))
+    
+    output = (output * 255).astype("uint8")
     return output
 
+sharp = np.array((
+	[0, -1, 0],
+	[-1, 5, -1],
+	[0, -1, 0]), dtype="int")
 
-img = imread("ill.png")
+blurr = np.array(([
+    [0.0625, 0.125, 0.0625],
+    [0.125, 0.25, 0.125],
+    [0.0625, 0.125, 0.0625]]),dtype=float)
 
-im = Image.open('ill.png')
+edge = np.array(([
+    [-1, -1, -1],
+    [-1, 8, -1],
+    [-1, -1, -1]
+]),dtype="int")
 
-pixel_val = list(im.getdata())
 
-img = np.asarray(im)
+img = cv2.imread("ill.png")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-print(img.shape)
+save = Image.fromarray(img)
+save.save('original.png')
 
-sharp = [
-    [0, -1, 0],
-    [-1, 5, -1],
-    [0, -1, 0]
-]
 
-s = apply(img, sharp)
+sharpend = convolve(img, sharp)
+blurred = convolve(img, blurr)
+edged = convolve(img, edge)
 
-save = Image.fromarray(np.uint8(s)).convert('RGB')
+save = Image.fromarray(sharpend)
 save.save('sharp.png')
+
+save = Image.fromarray(blurred)
+save.save('blurr.png')
+
+save = Image.fromarray(edged)
+save.save('edge.png')
+
+
+
